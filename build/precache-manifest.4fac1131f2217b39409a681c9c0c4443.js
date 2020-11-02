36c7cd500ebc13a5ba788e72c31b62451b38763a
DhanushThumanapally <68501265+DhanushThumanapally@users.noreply.github.com>
2020-11-03 00:39:47 +0530

    Add files via upload

---
+++ build/precache-manifest.4fac1131f2217b39409a681c9c0c4443.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b485870cd2b0731054826bb983c0f9b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d4a671e35fea602e519",
+    "url": "/static/css/main.9fa83195.chunk.css"
+  },
+  {
+    "revision": "b6742956d0d73730b709",
+    "url": "/static/js/2.ea59d149.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.ea59d149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4a671e35fea602e519",
+    "url": "/static/js/main.d3d1d803.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
